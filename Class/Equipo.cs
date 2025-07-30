3406053b1f6d68bb31c76fc86d77b8a7ebc5c8e0
Francis <francisco-bujonok@outlook.com>
2025-07-30 17:35:44 -0300

    Avance
    Cree algunas clases, lo sigo luego. Comprobaré si están bien

---
+++ Class/Equipo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Torneos
+{
+    public class Equipo
+    {
+        public string Nombre { get; set; }
+        public int Puntos { get; set; }
+        public int GolesAFavor { get; set; }
+        public int GolesEnContra { get; set; } // Changed type from string to int  
+        public string Ciudad { get; set; }
+
+        public Equipo(string nombre, int puntos, int golesafavor, int golesencontra, string ciudad)
+        {
+            this.Nombre = nombre;
+            this.Ciudad = ciudad;
+            this.Puntos = puntos;
+            this.GolesAFavor = golesafavor;
+            this.GolesEnContra = golesencontra;
+        }
+    }
+}
---
+++ Class/Partido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Torneos
+{
+    public class Partido
+    {
+        public string EquipoLocal { get; set; }
+        public string EquipoVisitante { get; set; }
+        public int GolesLocal { get; set; } 
+        public int GolesVisitante { get; set; }
+    }
+}
